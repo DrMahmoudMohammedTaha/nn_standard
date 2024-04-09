021c6ec4ef7a4e8e4f04ef07ea6cf16e7e51cdce
Mahmoud Taha <mahmoud.taha17@feng.bu.edu.eg>
2024-04-09 22:14:12 +0200

    good

--- data/read_resize_images.py
+++ data/read_resize_images.py
@@ -1,4 +1,4 @@
-
+## read and resized images
 # pip install opencv-python
 
 import glob
@@ -9,21 +9,24 @@ import os
 target_size = (128, 128)
 
 def read_resize_images(source_dir , categories):
-    temp_list = []
+    x_temp = []
+    y_temp = []
     for category in categories:
         for filepath in glob.glob(os.path.join(source_dir, category, "*")):
             label = 0 if category == "cats" else 1
             img = cv2.imread(filepath)
             img = cv2.resize(img, target_size)
-            temp_list.append((img, label))
+            img = np.array(img)
+            y_temp.append(label)
+            x_temp.append(img)
 
     print("folder: " + os.path.join(source_dir))
-    print(len(temp_list)) 
-    print(temp_list[0])   
-    return temp_list
+    print(len(x_temp)) 
+    print(x_temp[0])   
+    return x_temp , y_temp
 
 # source file, catefories list
-train_list = read_resize_images( "D:\\Artificial Intelligence\\Machine Learning\\_projects\\dogs_cats_classification\\data\\processed\\train", ["cat", "dog"])
-test_list = read_resize_images( "D:\\Artificial Intelligence\\Machine Learning\\_projects\\dogs_cats_classification\\data\\processed\\test", ["cat", "dog"])
-val_list = read_resize_images( "D:\\Artificial Intelligence\\Machine Learning\\_projects\\dogs_cats_classification\\data\\processed\\val", ["cat", "dog"])
+x_train , y_train = read_resize_images( "D:\\Artificial Intelligence\\Machine Learning\\_projects\\dogs_cats_classification\\data\\processed\\train", ["cat", "dog"])
+x_test , y_test = read_resize_images( "D:\\Artificial Intelligence\\Machine Learning\\_projects\\dogs_cats_classification\\data\\processed\\test", ["cat", "dog"])
+x_val , y_val = read_resize_images( "D:\\Artificial Intelligence\\Machine Learning\\_projects\\dogs_cats_classification\\data\\processed\\val", ["cat", "dog"])
 
